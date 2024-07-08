4e65d9e310b866cff0ec70422928aab8d61bed97
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com.>
2024-07-08 13:15:00 +0200

    style: Abbreviations
    Avoid abbreviations, making it easier to read the code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
-const tc = require('@actions/tool-cache');
-const cp = require('node:child_process');
+const toolCache = require('@actions/tool-cache');
+const childProcess = require('node:child_process');
 const os = require('node:os');
 
 const osMapper = {
@@ -22,28 +22,28 @@ const archMapper = {
 async function run() {
   try {
     const phrase = 'phrase'
-    const osPlat = os.platform();
+    const osPlatform = os.platform();
     const osArch = os.arch();
     const version = core.getInput('version');
-    const cacheToolPath = tc.find(phrase, version)
+    const cacheToolPath = toolCache.find(phrase, version)
 
     if (cacheToolPath && cacheToolPath !== '') {
       core.addPath(cacheToolPath);
       return;
     }
 
-    const fileName = isWindows(osPlat)
-      ? `phrase_${osMapper[osPlat]}_${archMapper[osArch]}.exe`
-      : `phrase_${osMapper[osPlat]}_${archMapper[osArch]}`
+    const fileName = isWindows(osPlatform)
+      ? `phrase_${osMapper[osPlatform]}_${archMapper[osArch]}.exe`
+      : `phrase_${osMapper[osPlatform]}_${archMapper[osArch]}`
 
     const downloadUrl = `https://github.com/phrase/phrase-cli/releases/download/${version}/${fileName}`;
-    const downloadPath = await tc.downloadTool(downloadUrl);
-    const toolPath = await tc.cacheFile(downloadPath, phrase, phrase, version, osArch);
+    const downloadPath = await toolCache.downloadTool(downloadUrl);
+    const toolPath = await toolCache.cacheFile(downloadPath, phrase, phrase, version, osArch);
 
     core.addPath(toolPath);
 
-    if (!isWindows(osPlat)) {
-      cp.exec(`chmod +x ${toolPath}/${phrase}`)
+    if (!isWindows(osPlatform)) {
+      childProcess.exec(`chmod +x ${toolPath}/${phrase}`)
     }
     core.setOutput(toolPath);
   } catch (error) {

8fff5826f406630503e14ce0faa87d8779cd44a7
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com.>
2024-07-08 13:16:48 +0200

    style: Formatting
    Format code base

--- index.js
+++ index.js
@@ -7,25 +7,25 @@ const osMapper = {
   win32: 'windows',
   darwin: 'macosx',
   linux: 'linux',
-}
+};
 
 function isWindows(osPlatform) {
-  return osPlatform === 'win32'
+  return osPlatform === 'win32';
 }
 
 const archMapper = {
   x32: '386',
   x64: 'amd64',
-  arm64: 'arm64'
-}
+  arm64: 'arm64',
+};
 
 async function run() {
   try {
-    const phrase = 'phrase'
+    const phrase = 'phrase';
     const osPlatform = os.platform();
     const osArch = os.arch();
     const version = core.getInput('version');
-    const cacheToolPath = toolCache.find(phrase, version)
+    const cacheToolPath = toolCache.find(phrase, version);
 
     if (cacheToolPath && cacheToolPath !== '') {
       core.addPath(cacheToolPath);
@@ -34,7 +34,7 @@ async function run() {
 
     const fileName = isWindows(osPlatform)
       ? `phrase_${osMapper[osPlatform]}_${archMapper[osArch]}.exe`
-      : `phrase_${osMapper[osPlatform]}_${archMapper[osArch]}`
+      : `phrase_${osMapper[osPlatform]}_${archMapper[osArch]}`;
 
     const downloadUrl = `https://github.com/phrase/phrase-cli/releases/download/${version}/${fileName}`;
     const downloadPath = await toolCache.downloadTool(downloadUrl);
@@ -43,7 +43,7 @@ async function run() {
     core.addPath(toolPath);
 
     if (!isWindows(osPlatform)) {
-      childProcess.exec(`chmod +x ${toolPath}/${phrase}`)
+      childProcess.exec(`chmod +x ${toolPath}/${phrase}`);
     }
     core.setOutput(toolPath);
   } catch (error) {
